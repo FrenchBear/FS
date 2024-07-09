bcb0c0e787202c9811b7dd4697785a9e546683a8
French Bear <FrenchBear38@outlook.com>
2024-07-09 16:56:13 +0200

    More F# learning code

--- Learning/12 Why/Program.fs
+++ Learning/12 Why/Program.fs
@@ -412,8 +412,10 @@ printfn ""
 // matching tuples directly
 let firstPart, secondPart, _ =  (1,2,3)  // underscore means ignore
 
+#nowarn "25"
 // matching lists directly
 let elem1::elem2::rest = [1..10]       // ignore the warning for now
+#warn "25"
 
 // matching lists inside a match..with
 let listMatcher aList =
@@ -451,3 +453,91 @@ printfn "The customer called %s has id %i" name2 id2
 let { Name=name3;  Address={Street=street3}  } =  customer1
 printfn "The customer is called %s and lives on %s" name3 street3
 
+
+// Matching simple type of a variable (ca&n't use a "int i" match case contrary to C#)
+let typ v =
+    match v with
+    | i when i.GetType()=typeof<int> -> printfn "Integer %A" i
+    | d when d.GetType()=typeof<double> -> printfn "Double %A" d
+    | _ -> printfn "other"
+
+typ 42
+typ (box 42)
+typ 42.5
+typ true
+
+// Doesn't work
+// let Test (o: 'T) =
+//     let res =
+//         match o with
+//         | :? int32 as i -> $"Integer {i}"
+//         | :? int16 as s -> $"Short {s}"
+//         | :? bool as b -> "xx"
+//         | _ -> "Other"
+//     printfn "%s" res
+// 
+// Test 42
+// Test (box 45)
+// Test 33s
+
+printfn ""
+
+
+// --------------------------------
+// Active Patterns
+
+let (|Int|_|) str =
+   match System.Int32.TryParse(str:string) with
+   | (true,int) -> Some(int)
+   | _ -> None
+
+let (|Bool|_|) str =
+   match System.Boolean.TryParse(str:string) with
+   | (true,bool) -> Some(bool)
+   | _ -> None
+
+// Once these patterns have been set up, they can be used as part of a normal “match..with” expression.
+let testParse str =
+    match str with
+    | Int i -> printfn "The value is an int '%i'" i
+    | Bool b -> printfn "The value is a bool '%b'" b
+    | _ -> printfn "The value '%s' is something else" str
+
+testParse "12"
+testParse "true"
+testParse "abc"
+
+
+// create an active pattern
+open System.Text.RegularExpressions
+let (|FirstRegexGroup|_|) pattern input =
+   let m = Regex.Match(input,pattern)
+   if (m.Success) then Some m.Groups.[1].Value else None
+
+let testRegex str =
+    match str with
+    | FirstRegexGroup "http://(.*?)/(.*)" host ->
+           printfn "The value is a url and the host is %s" host
+    | FirstRegexGroup ".*?@(.*)" host ->
+           printfn "The value is an email and the host is %s" host
+    | _ -> printfn "The value '%s' is something else" str
+
+testRegex "http://google.com/test"
+testRegex "alice@hotmail.com"
+
+
+// Active pattern with no parameters
+// FizzBuzz challenge written using active patterns
+let (|MultOf3|_|) i = if i % 3 = 0 then Some MultOf3 else None
+let (|MultOf5|_|) i = if i % 5 = 0 then Some MultOf5 else None
+
+let fizzBuzz i =
+  match i with
+  | MultOf3 & MultOf5 -> printf "FizzBuzz, "
+  | MultOf3 -> printf "Fizz, "
+  | MultOf5 -> printf "Buzz, "
+  | _ -> printf "%i, " i
+
+// test
+[1..20] |> List.iter fizzBuzz
+printfn "\n"
