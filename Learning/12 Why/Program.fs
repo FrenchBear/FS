171c97ca3b6fa7e747c210e663770a68348fac0d
French Bear <FrenchBear38@outlook.com>
2024-07-10 17:07:20 +0200

    More F#

--- Learning/12 Why/Program.fs
+++ Learning/12 Why/Program.fs
@@ -660,4 +660,64 @@ printfn ""
 
 
 // --------------------------------
+// Units of measure
+
+// define some measures
+[<Measure>]
+type cm
+
+[<Measure>]
+type inches
+
+[<Measure>]
+type feet =
+   // add a conversion function
+   static member toInches(feet : float<feet>) : float<inches> = feet * 12.0<inches/feet>
+
+// Add other conversion
+type cm with
+    static member toInches(cm: float<cm>): float<inches> = cm / 2.54<cm/inches>
+
+printfn "186 cm = %g inches" (cm.toInches(186.0<cm>))
+
+// define some values
+let meter = 100.0<cm>
+let yard = 3.0<feet>
+
+// convert to different measure
+let yardInInches = feet.toInches(yard)
+
+// can't mix and match!
+// yard + meter
+
+// now define some currencies
+[<Measure>]
+type GBP
+
+[<Measure>]
+type USD
+
+let gbp10 = 10.0<GBP>
+let usd10 = 10.0<USD>
+gbp10 + gbp10 |> ignore         // allowed: same currency
+// gbp10 + usd10                // not allowed: different currency
+// gbp10 + 1.0                  // not allowed: didn't specify a currency
+gbp10 + 1.0<_> |> ignore        // allowed using wildcard
+gbp10 + 1.0<GBP>  |> ignore     // Same unit, Ok!
+
+printfn ""
+
+
+
+// --------------------------------
+// Stop a type being compared
+
+// deny comparison
+[<NoEquality; NoComparison>]
+type CustomerAccount = {CustomerAccountId: int}
+
+let x = {CustomerAccountId = 1}
+
+// x = x       // error!
+x.CustomerAccountId = x.CustomerAccountId |> ignore // no error
 
