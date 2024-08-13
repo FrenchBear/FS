2a4e7613dee2656276d1327c722594b5b3ef6c86
French Bear <FrenchBear38@outlook.com>
2024-08-13 22:59:41 +0200

    Work on Elevator simulation

--- Learning/31 Ascenseur/Program.fs
+++ Learning/31 Ascenseur/Program.fs
@@ -17,8 +17,7 @@ type Landing = { Persons: Person list }
 
 type MotorState =
     | Stop
-    | Up
-    | Down
+    | Moving
     | Accelerating
     | Decelerating
 
@@ -28,32 +27,164 @@ type DoorState =
     | Opening
     | Closing
 
+type CabinState =
+    | Idle
+    | OpeningDoors
+    | DoorsOpen
+    | ClosingDoors
+    | Moving
+
+type DirectionState =
+    | NoDirection
+    | Up
+    | Down
+
 type Cabin =
     { StateTime: int
-      Position: float
+      Position: Floor
       Motor: MotorState
       Door: DoorState
+      Direction: DirectionState
+      Cabin: CabinState
       StopRequested: bool[]
-      Capacity: int
+      //Capacity: int
       Persons: Person list }
 
 
 let levels = 6 // 0=Ground, and levels 1..5
 let landings = Array.create levels { Landing.Persons = [] }
 
-let cabin =
+let cabinInitialState =
     { StateTime = 0
       Position = 0
       Motor = Stop
+      Direction = NoDirection
       Door = Closed
+      Cabin = Idle
       StopRequested = Array.create levels false
-      Capacity = 10
+      //Capacity = 10
       Persons = [] }
 
+let numberOfCabins = 1
+let cabins = Array.create numberOfCabins cabinInitialState
 
-module PersonsModule =
-    let personsToCarry = 15
-    let arrivalLength = 5 * 60
+
+type PersonEventDetail =
+    | Arrival of Person
+
+type PersonEvent =
+    { Clock: int
+      Event: PersonEventDetail 
+    }
+
+
+type ElevatorEventDetail =
+    | ElevatorOn
+    | EndAcceleration
+    | EndDeceleration
+    | EndMovingFullSpeed
+    | Decision
+    | EndOpeningDoors
+
+type ElevatorEvent =
+    { Clock: int
+      Event: ElevatorEventDetail }
+
+module ElevatorModule =
+    let elevatorQueue =
+        new System.Collections.Generic.PriorityQueue<ElevatorEvent, Clock>()
+
+    let evt =
+        { ElevatorEvent.Clock = 0
+          Event = ElevatorOn }
+
+    elevatorQueue.Enqueue(evt, evt.Clock)
+
+    let getNextElevatorEventClock () =
+        if elevatorQueue.Count = 0 then
+            None
+        else
+            let evt = elevatorQueue.Peek()
+            Some evt.Clock
+
+    let processEvent clk =
+        let evt = elevatorQueue.Dequeue()
+        printfn "Evevator.processEvent evt=%0A" evt
+        assert (clk = evt.Clock)
+
+        match evt.Event with
+        | ElevatorOn -> 
+            printfn "Elevator On and ready"
+        
+        | EndAcceleration ->
+            let evt =
+                { ElevatorEvent.Clock = clk + 2
+                  Event = Decision }
+            elevatorQueue.Enqueue(evt, evt.Clock)
+
+        | Decision ->
+            // Update Position
+            let cabin = cabins[0]
+            let newCabin = { cabin with Position = cabin.Position + if cabin.Direction=Up then 1 else -1 }
+            cabins[0] <- newCabin
+
+            // Decide if we stop at next floor or not
+            let evt =
+                if cabin.StopRequested[cabin.Position] then
+                    { ElevatorEvent.Clock = clk + 1
+                      Event = EndMovingFullSpeed }
+                else
+                    { ElevatorEvent.Clock = clk + 4
+                      Event = Decision }
+            elevatorQueue.Enqueue(evt, evt.Clock)
+
+        | EndMovingFullSpeed ->
+            let evt =
+                { ElevatorEvent.Clock = clk + 2
+                  Event = EndDeceleration }
+            elevatorQueue.Enqueue(evt, evt.Clock)
+
+        | EndDeceleration ->
+            // Ok, we arrive at a floor with stop requested
+            let evt =
+                { ElevatorEvent.Clock = clk + 2
+                  Event = EndOpeningDoors }
+            elevatorQueue.Enqueue(evt, evt.Clock)
+
+        | EndOpeningDoors ->
+            // ToDo
+            ()
+
+    let callElevator clk entry exit =
+        printfn "Calling elevator from level %d to go to level %d" entry exit
+        assert (exit <> entry)
+        let cabin = cabins[0]
+
+        if cabin.Cabin = Idle then
+            assert (cabin.Door = Closed)
+            assert (cabin.Motor = Stop)
+            cabin.StopRequested[exit] <- true
+
+            let newCabin = 
+                { cabin with
+                    Cabin = Moving
+                    Motor = Accelerating
+                    Direction = if (entry>exit) then Up else Down
+                }
+
+            cabins[0] <- newCabin
+            let evt =
+                { ElevatorEvent.Clock = clk + 2
+                  Event = EndAcceleration
+                }
+
+            elevatorQueue.Enqueue(evt, evt.Clock)
+
+
+
+module PersonModule =
+    let personsToCarry = 1
+    let arrivalLength = 60
     let rndPersons = new System.Random(1)
 
     let getRandomPerson () =
@@ -71,29 +202,72 @@ module PersonsModule =
           EntryTime = 0
           ExitTime = 0 }
 
-    let personsArray = [| for _ in 1..personsToCarry -> getRandomPerson () |]
-    let sortedPersonsArray = Array.sortBy (fun p -> p.ArrivalTime) personsArray
-    for p in sortedPersonsArray do
-        printfn "%0A" p
-    printfn ""
+    let personQueue = new System.Collections.Generic.PriorityQueue<PersonEvent, Clock>()
 
-    (* Could use a priority queue...
-    let personsQueue = new System.Collections.Generic.PriorityQueue<Person, Clock>()
-    for i in 0..personsToCarry-1 do
-        //let p = getRandomPerson()
-        let p = personsArray[i]
-        personsQueue.Enqueue(p, p.ArrivalTime)
+    for i in 0 .. personsToCarry - 1 do
+        let p = getRandomPerson ()
+        let evt = { PersonEvent.Clock = p.ArrivalTime 
+                    Event = Arrival p
+                  }
+        personQueue.Enqueue(evt, evt.Clock)
 
-    for i in 1..personsToCarry do
-        let p=personsQueue.Dequeue()
-        printfn "%0A" p
-    *)
+    let peekNextPersonEvent () =
+        if personQueue.Count = 0 then
+            None
+        else
+            let evt = personQueue.Peek()
+            Some(evt.Clock)
 
-    let mutable currentIndex = 0
+    let processEvent clk =
+        let evt = personQueue.Dequeue()
+        printfn "Evevator.processEvent evt=%0A" evt
+        assert (clk = evt.Clock)
 
-    let getNextEventClock() =
-        if currentIndex=personsToCarry
-        then None
-        else Some (sortedPersonsArray[currentIndex].ArrivalTime)
+        match evt.Event with
+        | Arrival p ->
+            let prevList = landings[p.EntryLevel].Persons
+            landings[p.EntryLevel] <- { Persons = p :: prevList }
+            ElevatorModule.callElevator clk p.EntryLevel p.ExitLevel
 
-    
+
+// Runs the simulation
+// In charge or master clock
+// Find next event in line, process it, and iterates until there are no more events to process
+module SchedulerModule =
+
+    let rec processNextEvent clk =
+        let nextPersonEventClock = PersonModule.peekNextPersonEvent ()
+        let nextElevatorEventClock = ElevatorModule.getNextElevatorEventClock ()
+        printfn "Scheduler.processNextEvent clk=%d pe=%0A ee=%0A" clk nextPersonEventClock nextElevatorEventClock
+
+        let nextClock =
+            match nextPersonEventClock, nextElevatorEventClock with
+            | None, None -> -1
+            | None, Some elevatorNextClock ->
+                assert (elevatorNextClock >= clk)
+                ElevatorModule.processEvent elevatorNextClock
+                elevatorNextClock
+            | Some personNextClock, None ->
+                assert (personNextClock >= clk)
+                PersonModule.processEvent personNextClock
+                personNextClock
+            | Some personNextClock, Some elevatorNextClock when personNextClock > elevatorNextClock ->
+                assert (elevatorNextClock >= clk)
+                ElevatorModule.processEvent elevatorNextClock
+                elevatorNextClock
+            | Some personNextClock, Some elevatorNextClock when personNextClock < elevatorNextClock ->
+                assert (personNextClock >= clk)
+                PersonModule.processEvent personNextClock
+                personNextClock
+            | Some personNextClock, Some elevatorNextClock ->
+                assert (personNextClock = elevatorNextClock)
+                assert (personNextClock >= clk)
+                PersonModule.processEvent personNextClock
+                ElevatorModule.processEvent elevatorNextClock
+                personNextClock
+
+        match nextClock with
+        | -1 -> printfn "Fin de la simulation clk=%d" clk
+        | _ -> processNextEvent nextClock
+
+    processNextEvent 0
